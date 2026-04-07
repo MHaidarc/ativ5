8fb03b4b799361d0671b470756fe704fdc295bc0
3036308 <3036308@itp.local@LABFMAQ05.itp.local>
2026-04-07 12:24:24 -0300

    segundo exercicio não terminado

--- src/main/java/com/ifsp/ativ5/Atividade5Controller.java
+++ src/main/java/com/ifsp/ativ5/Atividade5Controller.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Atividade5Controller {
+    
     public List<Personagem> personagens = new ArrayList<Personagem>();
-
+    
     @GetMapping("/listarPersonagens")
     public String listarPersonagens(Model model) {
 
@@ -21,4 +23,16 @@ public class Atividade5Controller {
 
         return "personagens.html";
     }
+
+    public List<Tarefa> tarefas = new ArrayList<Tarefa>();
+
+    @GetMapping("/listarTarefas")
+    public String listarTarefas(Model model) {
+
+        model.addAttribute("tarefas", tarefas);
+
+        return "tarefas.html";
+    }
+
+    //TODO: POSTMAPPING DAORINHA
 }
---
+++ src/main/java/com/ifsp/ativ5/Tarefa.java
@@ -0,0 +1,30 @@
+package com.ifsp.ativ5;
+
+public class Tarefa {
+    private String descricao;
+    private String data;
+    
+    public Tarefa() {
+    }
+
+    public Tarefa(String descricao, String data) {
+        this.descricao = descricao;
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
